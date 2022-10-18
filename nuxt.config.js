24e78444dde2fe9d4a8583763b6f57927be0880c
Asigri Shamsu-Deen Al-Heyr <deen@coldsis.com>
2022-10-18 18:17:38 +0000

    added page properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,4 +2,18 @@ export default {
 
     components: true,
 
+    head: {
+        titleTemplate: "BBnB: %s",
+        htmlAttrs: {
+            lang: "en"
+        },
+        bodyAttrs: {
+            class: ["my-style"]
+        },
+        meta: [{
+            charset: "utf-8",
+
+        }],
+    }
+
 }
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,18 @@
 import homes from '~/data/homes'
 
 export default {
+
+    head(){
+        return {
+            title: 'Homepage',
+            meta: [{
+                name: 'description',
+                content: 'This is the homepage',
+                hid: 'description'
+            }]
+        }
+    },
+
     data() {
         return {
             homes: homes.slice(0,3)
